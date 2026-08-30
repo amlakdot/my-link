01953b5f86978a814200f3237c6b0a19a81bf28a
amlakdot <xixlogin@gmail.com>
2026-08-30 22:35:05 +0330

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://jfhedbwermlacgjntfnr.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vhiU-ct3hvm9t2-lRG7ZIw_bgFBWlEQ";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
